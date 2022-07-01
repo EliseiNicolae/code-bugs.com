24f4c521275e5919553392c921ab2cc4ccb60c88
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2022-07-01 11:20:16 +0300

    Add lazy loading on markdown images.

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,7 +1,10 @@
 import fs from "fs";
 import matter from "gray-matter";
-import md from "markdown-it";
 import Layout from "@/components/Layout";
+
+const md = require("markdown-it")();
+const lazy_loading = require("markdown-it-image-lazy-loading");
+md.use(lazy_loading);
 
 export async function getStaticPaths() {
   const files = fs.readdirSync("posts");
@@ -31,9 +34,9 @@ export async function getStaticProps({ params: { slug } }: any) {
 export default function PostPage({ frontVariables, content, slug }: any) {
   return (
     <Layout seo={frontVariables}>
-      <div className="prose prose-sm sm:prose md:prose-lg  mx-auto">
+      <div className="prose prose-sm sm:prose d:prose-lg  mx-auto">
         <h1>{frontVariables?.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
+        <div dangerouslySetInnerHTML={{ __html: md.render(content) }} />
         <a
           href={`https://github.com/EliseiNicolae/code-bugs.com/blob/main/posts/${slug}.md`}>
           Edit on Github
