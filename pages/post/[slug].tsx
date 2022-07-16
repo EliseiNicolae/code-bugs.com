f7c3c6d5e7481699bb9fc0ed89122bf026c84740
Elisei.Nicolae <43387542+EliseiNicolae@users.noreply.github.com>
2022-07-16 19:40:26 +0300

    Update [slug].tsx

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,5 @@
+import Layout from "@/components/Layout";
+
+export default function PostPage({ frontVariables, content, slug }: any) {
+  return <Layout seo={frontVariables}>Redirect...</Layout>;
+}
